b2be0e3c7aae2e2d26ff8c568a410aeff2f2e7cf
Thálisson Gonçalves <thalissongsilva70@gmail.com>
2023-05-01 00:54:12 -0300

    Feito desafio da aula 27 da seção 03

---
+++ secao-03-Iniciando-com-programacao/aula-27-desafio-area-circunferencia.js
@@ -0,0 +1,11 @@
+// PI = 3.141592
+// PI * raio * raio
+
+let raio = 10;
+const PI = 3.141592;
+const resultado = PI * raio * raio;
+const resultado2 = Math.PI * raio * raio;
+
+console.log('O valor da área da circunferência é ' + resultado + 'm²');
+
+console.log('O valor da área da circunferência é ' + resultado2 + 'm²');
