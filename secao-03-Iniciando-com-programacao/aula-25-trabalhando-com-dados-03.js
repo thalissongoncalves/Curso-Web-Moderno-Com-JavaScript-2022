99edb9e803359d58f04f5b805e62c164ccf19dbf
Thálisson Gonçalves <thalissongsilva70@gmail.com>
2023-05-01 00:39:33 -0300

    Feito aula 25 da seção 03

---
+++ secao-03-Iniciando-com-programacao/aula-25-trabalhando-com-dados-03.js
@@ -0,0 +1,12 @@
+let idade = 31;
+console.log(typeof -31); // number
+console.log(typeof idade); // number
+
+let salario = 4578.32; // number
+console.log(typeof salario); // number
+
+let estaChovendo = true; // boolean
+console.log(typeof estaChovendo); // boolean
+
+console.log(typeof "Teste"); // string
+console.log(typeof 'Teste'); // string
