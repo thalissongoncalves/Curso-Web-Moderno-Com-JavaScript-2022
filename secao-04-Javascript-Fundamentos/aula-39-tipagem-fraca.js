891c564ec974f6b80ef33d64fb7e4ab8af81a4ee
Thálisson Gonçalves <thalissongsilva70@gmail.com>
2023-05-01 01:09:59 -0300

    Feito aula 39 - tipagem fraca

---
+++ secao-04-Javascript-Fundamentos/aula-39-tipagem-fraca.js
@@ -0,0 +1,12 @@
+let qualquer = 'Legal';
+console.log(qualquer); // Legal
+console.log(typeof qualquer); // String
+
+qualquer = 3.1516;
+console.log(qualquer); // 3.1516
+console.log(typeof qualquer); // Number
+
+// Evitar nome genéricos e siglas
+let valor = '';
+let numero = 1;
+let pqp = false; // Produto Químico Perigoso... kkkk
