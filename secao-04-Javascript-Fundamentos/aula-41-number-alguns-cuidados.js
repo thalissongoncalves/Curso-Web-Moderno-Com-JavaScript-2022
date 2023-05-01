5121c1adb5cae480ba3622009a0fa13f10fa805a
Thálisson Gonçalves <thalissongsilva70@gmail.com>
2023-05-01 02:26:07 -0300

    Feito aula 44 - usando template literals

--- secao-04-Javascript-Fundamentos/aula-41-number-alguns-cuidados.js
+++ secao-04-Javascript-Fundamentos/aula-41-number-alguns-cuidados.js
@@ -1,5 +1,7 @@
 console.log(7 / 0); // Infinity
 console.log("10" / 2); // NaN
+console.log('3' + 2) // 32 (ocorre a concatenação)
+console.log('3' - 2) // 1 (não ocorre a concatenação)
 console.log("Show!" * 2); // NaN
 console.log(0.1 + 0.7); // 0.7999999999999999
 // console.log(10.toString())
---
+++ secao-04-Javascript-Fundamentos/aula-44-template-strings.js
@@ -0,0 +1,9 @@
+const nome = 'Rebeca';
+const concatenacao = 'Olá ' + nome + '!'; // Olá Rebeca!
+const template = `Olá ${nome}!`; // Olá Rebeca!
+
+// expressoes...
+console.log(`1 + 1 = ${1 + 1}`);
+
+const up = (texto) => texto.toUpperCase();
+console.log(`Ei... ${up('cuidado')}!`);
