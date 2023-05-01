759323063b01377ec8c39638996403391ebffeaf
Thálisson Gonçalves <thalissongsilva70@gmail.com>
2023-05-01 03:14:09 -0300

    Feito aula 52 - declaracoes de variaveis com var

---
+++ secao-04-Javascript-Fundamentos/aula-52-declaracoes-de-variaveis-com-var.js
@@ -0,0 +1,19 @@
+{
+    {
+        {
+            {
+                var sera = 'Será???'
+                console.log(sera) // Será???
+            }
+        }
+    }
+}
+console.log(sera) // Será???
+
+function teste() {
+    var local = 123
+    console.log(local) // 123
+}
+
+teste() // 123
+console.log(local) // Erro!
